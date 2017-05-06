7a326832826e81180e3fb00cad7056baeaf685be
Felipe Zapata <fzapata@Felipes-MacBook-Air.local>
2017-05-06 16:06:19 -0700

    added files

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,18 @@
-var express = require("express");
-var methodOverride = require("method-override");
-var bodyParser = require("body-parser");
-
-var app = express();
-var port = 3000;
-
-app.use(express.static(__dirname + "/public"));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 var mysql = require("mysql");
-
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+    port: 4000,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
+
+module.exports = connection;
